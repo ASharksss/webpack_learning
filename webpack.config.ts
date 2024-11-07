6517b438764030a4f029ae59d6e9290fe5e6d185
Alsu Kurbanalieva <alsukurbanalieva035@gmail.com>
2024-11-07 10:46:36 +0300

    decomposition webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,37 +1,25 @@
 import  path from 'path'
 import  HTMLWebpackPlugin from "html-webpack-plugin"
 import  webpack from "webpack"
+import {buildPlugins} from "./config/build/buildPlugins";
+import {buildLoaders} from "./config/build/buildLoaders";
+import {buildResolvers} from "./config/build/buildResolvers";
+import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
+import {BuildPaths} from "./config/build/types/config";
 
-// const path = require('path')
-// const HTMLWebpackPlugin = require('html-webpack-plugin')
-// const webpack = require('webpack')
-
-const config: webpack.Configuration = {
-  mode: 'development',
-  entry: path.resolve(__dirname, 'src', 'index.ts'),
-  output: {
-    filename: "[name].[contenthash].js", // Дабы избежать ошибок с кешированием.
-    path: path.resolve(__dirname, 'build'),
-    clean: true
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: path.resolve(__dirname, 'public', 'index.html')
-    }),
-    new webpack.ProgressPlugin()
-  ],
-  module: {
-    rules: [ // loader'ы для обработки файлов вне JS (ts, изображения, видео и пр.)
-      {
-        test: /\.tsx?$/, // Регулярные выражения для отлавливания
-        use: 'ts-loader', // Используемый loader
-        exclude: /node_modules/, // Исключаем из проверки node_modules
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'] //Файлы, у которых при импорте не будем указывать расширение
-  }
+const paths: BuildPaths = {
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    build: path.resolve(__dirname, 'build'),
+    html: path.resolve(__dirname, 'public', 'index.html')
 }
+
+const mode = 'development';
+const isDev = mode === 'development'
+
+const config: webpack.Configuration = buildWebpackConfig({
+    mode: 'development',
+    paths,
+    isDev
+})
 
 export default config
